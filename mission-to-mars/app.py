873d7feaa55654a3b6936a1558c24d21cfc2f207
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-02-22 19:33:01 -0500

    making code more efficient

--- mission-to-mars/app.py
+++ mission-to-mars/app.py
@@ -24,14 +24,7 @@ def home():
         nasa_pp = mars_data['nasa_news']['news_pp'],
         jpl_img = mars_data['jpl_featured_image'],
         table = mars_data['mars_facts_table'],
-        hemi1title = mars_data['hemisphere_image_urls'][0]['title'],
-        hemi1url = mars_data['hemisphere_image_urls'][0]['img_url'],
-        hemi2title = mars_data['hemisphere_image_urls'][1]['title'],
-        hemi2url = mars_data['hemisphere_image_urls'][1]['img_url'],
-        hemi3title = mars_data['hemisphere_image_urls'][2]['title'],
-        hemi3url = mars_data['hemisphere_image_urls'][2]['img_url'],
-        hemi4title = mars_data['hemisphere_image_urls'][3]['title'],
-        hemi4url = mars_data['hemisphere_image_urls'][3]['img_url'],
+        hemidata = mars_data['hemisphere_image_urls']
         )
 
 # scrape route
